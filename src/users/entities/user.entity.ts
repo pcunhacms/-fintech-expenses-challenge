c368994aa0a4ca0000f4540c9c76fb9f4489db55
Gabriella Not <gabriellavictoria2819@gmail.com>
2026-06-16 22:53:22 -0300

    feat(auth): implementando autenticacao JWT login/register

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  name!: string;
+
+  @Column({
+    unique: true,
+  })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
