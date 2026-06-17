fa74d748b8787b5af84db42b72c79b12a02eb91f
Pedro Cunha <pedrohcunhams@gmail.com>
2026-06-17 18:50:06 -0300

    configurando frontend com react, typescript, shadcn, axios, zustand e react-router

---
+++ gf-case-web/src/routes/index.tsx
@@ -0,0 +1,16 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import { Login } from '@/api/pages/Login/Login'
+import { Dashboard } from '@/api/pages/Dashboard/Dashboard'
+
+export function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        
+        </BrowserRouter>
+    )
+}
